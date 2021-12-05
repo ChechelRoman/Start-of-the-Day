9612c962f5d01d9dfced3fba433d246525b0d70f
Chechel Roman <chechel-roman@lenta.ru>
2021-12-05 09:59:07 +0700

    refactor: refactor App component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WeatherPage } from './src/components/WeatherPage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WeatherPage/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
